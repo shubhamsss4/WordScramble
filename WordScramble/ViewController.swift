9230f5d4a46d8b222d0ef75588a16c61c1fe32a9
shubham shah <shubhamshekharshah@gmail.com>
2020-03-01 13:42:13 +0530

    Day 2 Completed

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -62,7 +62,66 @@ class ViewController: UITableViewController {
     }
     
     func submit(_ answer: String) {
+        let loweranswer = answer.lowercased()
         
+        let errorTitle: String
+        let errorMessage: String
+        
+        if isPossible(word: loweranswer) {
+            if isOriginal(word: loweranswer) {
+                if isReal(word: loweranswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                }
+                else {
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
+                }
+            }
+            else {
+                errorTitle = "Word already used"
+                errorMessage = "Be more original!"
+            }
+        }
+        else {
+            guard let title = title else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title.lowercased())."
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac,animated: true)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        guard var tempword = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempword.firstIndex(of: letter) {
+                tempword.remove(at: position)
+            }
+            else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
     }
     
     
